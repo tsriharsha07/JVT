69d14a2942ef435db5d20aa32088e7ff81d5c04a
tsriharsha07 <tsriharsha07@gmail.com>
2022-07-27 18:09:52 +0530

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,47 +10,48 @@ import { Badge } from '@mui/material';
 
 
 
+
+
 const Navbar = () => {
-    const state=useSelector((state)=> state.handleCart)
+  const state = useSelector((state) => state.handleCart)
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-white bg-success bg-opacity-25">
-        
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <nav className="navbar navbar-expand-lg navbar-white bg">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mk h5 text-white">
-                <li className="nav-item ">
-                <Link to='/' underline='none'  className='text-decoration-none'><p className="nav-link text-white text-decoration-none">Logo </p></Link>
-                </li>
-                <li className="nav-item">
-                <Link to='/men' className='text-decoration-none'><p className="nav-link text-white">Men</p></Link>
-                </li>
-                <li className="nav-item">
-                <Link to='/women' className='text-decoration-none'><p className="nav-link text-white">Women</p></Link>
-                </li>
-		        {/*    <li className="nav-item">
-                    <Link to='/kids'><p className="nav-link px-2 text-white">Kids</p></Link>
-                </li> */}
-                <li className="nav-item">
-                <Link to='/footwear' className='text-decoration-none'><p className="nav-link text-white ">Footwear</p></Link>
-                </li>
+          <ul className="navbar-nav mk h5 text-white">
+            <li className="nav-item ">
+              <Link to='/' underline='none' className='text-decoration-none'><p className="nav-link text-white text-decoration-none">Logo </p></Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/men' className='text-decoration-none'><p className="nav-link text-white">Men</p></Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/women' className='text-decoration-none'><p className="nav-link text-white">Women</p></Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link to='/footwear' className='text-decoration-none'><p className="nav-link text-white ">Footwear</p></Link>
+            </li>
 
 
-            </ul>
-            <form className="form-inline my-2 my-lg-0 d-flex flex-row">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <Link to='/'><button className="btn btn-outline-warning mx-2" type="submit"><SearchIcon className='align-items-center btn-outline-warning'/></button></Link>
-            </form>
-            <div className="mx-2 ">
-                <Link to='/login'><button className='btn btn-outline-warning m-2'>Login</button></Link>
-                <Link to='/signup'><button className='btn btn-outline-warning m-2'>SignUp</button></Link>
-                <Link to='/cart'><button className='btn btn-outline-warning m-2'><Badge><ShoppingCartIcon/>{state.length}</Badge></button></Link>
-            </div>
+          </ul>
+          <form className="form-inline my-2 my-lg-0 d-flex flex-row">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <Link to='/'><button className="btn btn-outline-warning mx-2" type="submit"><SearchIcon className='align-items-center btn-outline-warning' /></button></Link>
+          </form>
+          <div className="mx-2 d-flex flex-row">
+            <Link to='/login'><button className='btn btn-outline-warning m-2'>Login</button></Link>
+            <Link to='/signup'><button className='btn btn-outline-warning m-2'>SignUp</button></Link>
+            <Link to='/cart'><button className='btn btn-outline-warning m-2'><Badge><ShoppingCartIcon />{state.length}</Badge></button></Link>
+
+          </div>
         </div>
-    </nav>
+      </nav>
     </>
   )
 }
